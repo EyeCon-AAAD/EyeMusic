6dde594937912dba6d1689473fcf5c01765740e8
David Timbwa <davidtimbwa57@gmail.com>
2021-06-07 16:19:50 +0300

    ADD: Model inference with one input image

--- app/src/main/java/com/projectx/eyemusic/Model/OriginalModel.java
+++ app/src/main/java/com/projectx/eyemusic/Model/OriginalModel.java
@@ -2,11 +2,13 @@
  * Author: David T. Auna */
 package com.projectx.eyemusic.Model;
 
-import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.camera.core.impl.CaptureProcessor;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -18,9 +20,21 @@ import com.projectx.eyemusic.App;
 import com.projectx.eyemusic.Features.Feature1;
 import com.projectx.eyemusic.R;
 
+import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.Tensor;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OriginalModel {
 
@@ -51,9 +65,47 @@ public class OriginalModel {
     public GazePoint Predict(Feature1 feature){
         if(isModelDownloaded()){
             // DO prediction
-            Toast.makeText(App.getContext(), "Model downloaded",
-                    Toast.LENGTH_SHORT).show();
-            Log.d(TAG, "File: " + modelFile.getAbsolutePath());
+            // declare tensors
+            
+            TensorImage eyeLeftTensor = new TensorImage(DataType.UINT8);
+            TensorImage eyeRightTensor = new TensorImage(DataType.UINT8);
+            TensorImage faceTensor = new TensorImage(DataType.UINT8);
+            ByteBuffer faceMask = getFaceMask();
+
+            // load and pre-process images
+            eyeLeftTensor = processAndLoadImage(feature.getEyeLeftImage(), eyeLeftTensor, false);
+            eyeRightTensor = processAndLoadImage(feature.getEyeRightImage(), eyeRightTensor, false);
+            faceTensor = processAndLoadImage(feature.getFaceImage(), faceTensor, false);
+
+
+            // create container for prediction result
+            TensorBuffer coordinateBuffer = TensorBuffer.createFixedSize(new int[]{1, 2},
+                    DataType.FLOAT32);
+
+            // order of inputs matters
+            Object[] inputs = new Object[]{eyeLeftTensor.getBuffer(),
+                    eyeRightTensor.getBuffer(),
+                    faceTensor.getBuffer(),
+                    faceMask};
+            // outputs
+            Map<Integer, Object> output = new HashMap<>();
+            output.put(0, coordinateBuffer.getBuffer());
+
+            // infer
+            try{
+                interpreter.runForMultipleInputsOutputs(inputs, output);
+                // need to test inference
+                ByteBuffer out = coordinateBuffer.getBuffer();
+                out.rewind();
+                float x_coordinate = out.get(0);
+                float y_coordinate = out.get(1);
+                Log.d(TAG, String.format("Coordinates (x, y) : (%1.4f, %1.4f)", x_coordinate, y_coordinate));
+
+                return new GazePoint(x_coordinate, y_coordinate);
+            }catch (IllegalArgumentException e){
+                e.printStackTrace();
+                Log.e(TAG, e.getMessage());
+            }
             return null;
         }
         return null;
@@ -73,8 +125,6 @@ public class OriginalModel {
                         // The CustomModel object contains the local path of the model file,
                         // which you can use to instantiate a TensorFlow Lite interpreter.
                         Log.d(TAG, "Model downloaded!");
-                        Toast.makeText(App.getContext(), "Model downloaded",
-                                Toast.LENGTH_SHORT).show();
 
                         setModelDownloaded(true);
                         setModelFile(customModel.getFile());
@@ -102,7 +152,83 @@ public class OriginalModel {
         return isModelDownloaded;
     }
 
-    public void setModelDownloaded(Boolean modelDownloaded) {
+    private void setModelDownloaded(Boolean modelDownloaded) {
         isModelDownloaded = modelDownloaded;
+    }
+
+    private TensorImage processAndLoadImage(Bitmap image, TensorImage tensorImage, boolean isFaceMask){
+        // should be 64x64
+        float mean = getMean(image);
+        // pre-processing pipeline
+        ImageProcessor imageProcessor;
+        if(!isFaceMask) {
+            imageProcessor = new ImageProcessor.Builder()
+                    // Resize with Bilinear method
+                    .add(new ResizeOp(64, 64, ResizeOp.ResizeMethod.BILINEAR))
+                    // not sure if this normalizes to [0.0 - 1.0]
+                    .add(new NormalizeOp(mean, 127.0f))
+                    .build();
+        }else{
+            imageProcessor = new ImageProcessor.Builder()
+                    // Resize with Bilinear method
+                    .add(new ResizeOp(625, 1, ResizeOp.ResizeMethod.BILINEAR))
+                    // not sure if this normalizes to [0.0 - 1.0]
+                    .add(new NormalizeOp(mean, 127.0f))
+                    .build();
+        }
+        // load bitmap image to tensor
+        tensorImage.load(image);
+
+        // resize and normalize image
+
+        tensorImage = imageProcessor.process(tensorImage);
+        return tensorImage;
+    }
+
+    private float getMean(Bitmap image) {
+        float sum = 0.0f, mean;
+        float totalPixels = 64.0f * 64.0f * 3.0f;
+        for (int y = 0; y < image.getWidth(); y++) {
+            for (int x = 0; x < image.getHeight(); x++) {
+                int px = image.getPixel(x, y);
+
+                // get channel values fom the pixel value
+                int r = Color.red(px);
+                int g = Color.green(px);
+                int b = Color.blue(px);
+
+                sum += ((r / 255.0f) + (g / 255.0f) + (b / 255.0f));
+            }
+        }
+        mean = sum / totalPixels;
+        return mean;
+    }
+
+    // need to explicitly create ByteBuffer for face mask to match input shape for model
+    // used ALPHA_8
+    private ByteBuffer getFaceMask(){
+        Bitmap bitmap = Bitmap.createBitmap(25, 25, Bitmap.Config.ALPHA_8);
+        ByteBuffer faceMaskInput = ByteBuffer.allocateDirect(25 * 25 * 4).order(ByteOrder.nativeOrder());
+        int c = 0;
+        for (int y = 0; y < 25; y++) {
+            for (int x = 0; x < 25; x++) {
+                if(x % 2 == 0 && y % 2 == 0){
+                    bitmap.setPixel(x, y, Color.alpha(50));
+                    Log.d(TAG, "pixel val: " + bitmap.getPixel(x, y));
+                }
+                else{
+                    bitmap.setPixel(x, y, Color.alpha(255));
+                    Log.d(TAG, "pixel val: " + bitmap.getPixel(x, y));
+                }
+
+                int px = bitmap.getPixel(x, y);
+                int a = Color.alpha(px);
+                // Log.d(TAG, "Face mask px: " + px);
+                faceMaskInput.putFloat(px);
+                c++;
+            }
+        }
+        Log.d(TAG, "pixel count: " + c);
+        return faceMaskInput;
     }
 }
