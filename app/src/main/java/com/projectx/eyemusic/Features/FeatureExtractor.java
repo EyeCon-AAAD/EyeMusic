87ceaa1ee90be640542ded68383608b93b88552e
AisanSisani <aisan.sisani@gmail.com>
2021-06-09 17:19:56 +0300

    Fixed: if feature is null don't send it to predict or calibration.

--- app/src/main/java/com/projectx/eyemusic/Features/FeatureExtractor.java
+++ app/src/main/java/com/projectx/eyemusic/Features/FeatureExtractor.java
@@ -19,6 +19,11 @@ public class FeatureExtractor {
     public static void getData(RawFeature rawFeature){
          Feature1 newFeature = new Feature1(rawFeature.getOriginal(), rawFeature.getFace());
 
+         // TODO: complete show the error message that the face is not in the image so fix it
+         if (!newFeature.isFaceInImage()){
+             return;
+         }
+
          if (calibrationMode){
              MainActivity.getGraphicOverlayGazeLocation().clear();
              CalibrationRunnable.setNewFeature(newFeature);
