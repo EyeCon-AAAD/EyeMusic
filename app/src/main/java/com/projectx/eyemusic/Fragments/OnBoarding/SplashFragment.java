c9f10b1c790572dd650b3dffccdfb4ad9eb0d5bf
David Timbwa <davidtimbwa57@gmail.com>
2021-06-17 14:02:06 +0300

    ADD: On-Boarding Screens. Functionality still in development

---
+++ app/src/main/java/com/projectx/eyemusic/Fragments/OnBoarding/SplashFragment.java
@@ -0,0 +1,57 @@
+package com.projectx.eyemusic.Fragments.OnBoarding;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.projectx.eyemusic.MainActivity;
+import com.projectx.eyemusic.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SplashFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // wait a few seconds after showing the splash screen
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(isOnboardingFinished()){
+                    // start mainActivity
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }else{
+                    // navigate to the viewpager fragment
+                    NavHostFragment.findNavController(SplashFragment.this)
+                            .navigate(R.id.action_splashFragment_to_viewPagerFragment);
+                }
+            }
+        }, 3000);
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    private boolean isOnboardingFinished(){
+        SharedPreferences preferences = requireActivity().getSharedPreferences("onBoarding",
+                Context.MODE_PRIVATE);
+        return preferences.getBoolean("finished", false);
+    }
+}
---
+++ app/src/main/java/com/projectx/eyemusic/Fragments/OnBoarding/SpotifyConnectionFragment.java
@@ -0,0 +1,92 @@
+package com.projectx.eyemusic.Fragments.OnBoarding;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.projectx.eyemusic.Authentication.Authentication;
+import com.projectx.eyemusic.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SpotifyConnectionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SpotifyConnectionFragment extends Fragment {
+    Button btn_connect;
+    ViewPager2 viewPager2;
+
+    private Authentication authentication;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SpotifyConnectionFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SpotifyConnectionFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SpotifyConnectionFragment newInstance(String param1, String param2) {
+        SpotifyConnectionFragment fragment = new SpotifyConnectionFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_spotify_connection, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_connect = view.findViewById(R.id.btn_fragment_spotify_connect);
+        viewPager2 = getActivity().findViewById(R.id.view_pager);
+        btn_connect.setOnClickListener(view1 -> {
+            // navigate to the Permissions fragment(through view pager) for now
+            // TODO connection
+            viewPager2.setCurrentItem(1, true);
+        });
+    }
+
+
+}
