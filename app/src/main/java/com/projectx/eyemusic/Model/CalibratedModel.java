c9b83efecdf7b4e7147a75684185b30e3052b226
AisanSisani <aisan.sisani@gmail.com>
2021-07-05 19:45:45 +0300

    CHANGE: CalibratedModel.java includes normalization

--- app/src/main/java/com/projectx/eyemusic/Model/CalibratedModel.java
+++ app/src/main/java/com/projectx/eyemusic/Model/CalibratedModel.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 //TODO: check for trained
 public class CalibratedModel {
@@ -13,6 +14,9 @@ public class CalibratedModel {
     private CalibrationError trainingError = null;
     private boolean trained = false;
 
+    private Integer initialTrainSampleSize;
+    private Integer normalizedTrainSampleSize;
+
     CalibratedModel(){
         x_model = new LinearRegressionModel();
         y_model = new LinearRegressionModel();
@@ -20,6 +24,12 @@ public class CalibratedModel {
     }
 
     CalibratedModel(List<GazePoint> predictions, List <GazePoint> coordinates){
+        Objects.requireNonNull(predictions, "the prediction argument to Calibrated Model Constructor must not be null");
+        Objects.requireNonNull(coordinates, "the coordinates argument to Calibrated Model Constructor must not be null");
+        if (predictions.size() != coordinates.size()){
+            throw new IllegalArgumentException("the arguments to the Calibrated Model Constructor should be of same size");
+        }
+
         List<Float> x_array = new ArrayList<Float>();
         List<Float> y_array = new ArrayList<Float>();
         List<Float> x_prediction_array = new ArrayList<Float>();
@@ -35,9 +45,28 @@ public class CalibratedModel {
             y_array.add(coordinate.getY());
         }
 
-        x_model = new LinearRegressionModel(x_prediction_array, y_prediction_array, x_array, LinearRegressionModel.TRAIN_NORM_EQUATION);
-        y_model = new LinearRegressionModel(x_prediction_array, y_prediction_array, y_array, LinearRegressionModel.TRAIN_NORM_EQUATION);
+        //adding normalizing
+        List<Integer> x_prediction_outliers = getOutliersIndex(x_prediction_array);
+        List<Integer> y_prediction_outliers = getOutliersIndex(y_prediction_array);
+        List<Integer> outliers_indexes = new ArrayList<>(x_prediction_outliers);
+        outliers_indexes.addAll(y_prediction_outliers);
 
+        //deleting the outliers
+        List<Float> normalized_x_array = deleteOutliers(x_array, outliers_indexes);
+        List<Float> normalized_y_array = deleteOutliers(y_array, outliers_indexes);
+        List<Float> normalized_x_prediction_array = deleteOutliers(x_prediction_array, outliers_indexes);
+        List<Float> normalized_y_prediction_array = deleteOutliers(y_prediction_array, outliers_indexes);
+
+        // setting the size of the initial and normalized train samples
+        normalizedTrainSampleSize = normalized_x_prediction_array.size();
+        initialTrainSampleSize = x_prediction_array.size();
+
+
+        //training the model
+        x_model = new LinearRegressionModel(x_prediction_array, normalized_x_prediction_array, normalized_x_array, LinearRegressionModel.TRAIN_NORM_EQUATION);
+        y_model = new LinearRegressionModel(x_prediction_array, normalized_y_prediction_array, normalized_y_array, LinearRegressionModel.TRAIN_NORM_EQUATION);
+
+        //setting the training error metrics
         if (!x_model.isTrained() || !y_model.isTrained()){
             trainingError = null;
             trained = false;
@@ -45,17 +74,32 @@ public class CalibratedModel {
         else{
             trained = true;
             // predicting the predictions once again to find the error
-            List<GazePoint> calibPredictions = new ArrayList<GazePoint>();
-            for (GazePoint prediction: predictions){
-                calibPredictions.add(predict(prediction));
+            List<GazePoint> normalized_calibration_predictions = new ArrayList<>();
+            List<GazePoint> normalized_predictions = deleteOutliers(predictions, outliers_indexes);
+            List<GazePoint> normalized_coordinates = deleteOutliers(coordinates, outliers_indexes);
+
+            for (GazePoint prediction: normalized_predictions){
+                normalized_calibration_predictions.add(predict(prediction));
             }
 
-            trainingError = new CalibrationError(coordinates, calibPredictions);
+            trainingError = new CalibrationError(normalized_coordinates, normalized_calibration_predictions);
         }
 
     }
 
+    //for deleting the outliers from the list passed
+    private <T> List<T> deleteOutliers(List<T> initialList, List<Integer> outlierIndexes){
+        List<T> normalizedList = new ArrayList<>();
+        for(int i=0; i<initialList.size(); i++){
+            if(!outlierIndexes.contains(i)){
+                normalizedList.add(initialList.get(i));
+            }
+        }
+        return normalizedList;
+    }
 
+
+    // finding the outliers in a list
     private List<Integer> getOutliersIndex(List<Float> input) {
         if(input==null) return null;
 
@@ -63,8 +107,8 @@ public class CalibratedModel {
         if(input.size()<2){
             return null;
         }
-        List<Integer> outliers_index = new ArrayList<Integer>();
-        List<Float> sorted_input= new ArrayList<Float>(input);
+        List<Integer> outliers_index = new ArrayList<>();
+        List<Float> sorted_input= new ArrayList<>(input);
         List<Float> data1;
         List<Float> data2;
 
@@ -115,6 +159,14 @@ public class CalibratedModel {
         if(!trained)
             return null;
         return trainingError;
+    }
+
+    public Integer getInitialTrainSampleSize(){
+        return initialTrainSampleSize;
+    }
+
+    public Integer getNormalizedTrainSampleSize() {
+        return normalizedTrainSampleSize;
     }
 
     public boolean isTrained() {
